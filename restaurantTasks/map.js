745a07b9deb7866b1471ba4a06b2e293da2bb06e
GregTish <tishchuk167@gmail.com>
2026-03-27 19:15:17 +0200

    started design for restaurant tasks: home page, login and register, map integration and menu page + styles

---
+++ restaurantTasks/map.js
@@ -0,0 +1,6 @@
+const map = L.map('map').setView([60.1699, 24.9384], 11);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
