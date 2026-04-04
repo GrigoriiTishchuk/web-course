c9a8aaf73e6f3dcccfcf3d640831833a4bd1c39f
GregTish <tishchuk167@gmail.com>
2026-04-04 23:54:47 +0300

    assignment 4 is done

---
+++ week3/assignment4/src/api/routes/cat-router.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import multer from 'multer';
+import { createThumbnail } from '../../middlewares/upload.js';
+const upload = multer({dest: 'uploads/'});
+
+import {
+  getCat,
+  getCatById,
+  postCat,
+  putCat,
+  deleteCat,
+} from '../controllers/cat-controller.js';
+
+const catRouter = express.Router();
+
+catRouter.route('/').get(getCat).post(upload.single('cat'), createThumbnail, postCat);
+
+catRouter.route('/:id').get(getCatById).put(putCat).delete(deleteCat);
+
+export default catRouter;
